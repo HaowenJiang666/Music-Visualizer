2ae63c487858129adc86b02d4970f01df93a4171
Haowen Jiang <hj323@cornell.edu>
2022-07-14 19:40:25 -0400

    upload the source code of clock project

---
+++ batch_load.m
@@ -0,0 +1,16 @@
+function collections = batch_load(path, varargin)
+collections = {};
+files = dir(path);
+for index = 1:length(files)
+    file = files(index);
+    if strcmpi(file.name, '.') || strcmpi(file.name, '..')
+        continue;
+    end
+    if ~file.isdir
+        if isempty(varargin) || endsWith(file.name, varargin,'IgnoreCase',true)
+            collections = [collections; fullfile(path, file.name)];
+        end
+    else
+        collections = [collections; batch_load(fullfile(path, file.name), varargin{:})];
+    end
+end
